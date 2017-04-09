93f597bee9c6a4fa8d8b37cff5e9a9c5c4f1ad93
Matt Light <matt.light@lightdatasys.com>
2017-04-09 13:44:19 -0700

    Make anchorable links work on mobile resolutions

--- assets/main.js
+++ assets/main.js
@@ -1,21 +1,26 @@
 (function() {
-  function makeHeaderAnchor(idFinderCallback) {
+  function makeHeaderAnchor(sizeClass, idFinderCallback) {
     return function() {
       var $a = $('<a class="header-anchor"><i class="fa fa-paragraph"></i></a>');
+      $a.addClass(sizeClass);
       $a.attr('href', '#' + idFinderCallback($(this)));
 
       return $a;
     };
   }
 
-  $('h1[id],h2[id],h3[id],h4[id],h5[id],h6[id]').prepend(
-    makeHeaderAnchor(function($header) {
+  $('h1[id],h2[id],h3[id],h4[id],h5[id],h6[id]')
+    .prepend(makeHeaderAnchor('hidden-sm-down', function($header) {
       return $header.attr('id');
-    })
-  );
-  $('.anchorable').prepend(
-    makeHeaderAnchor(function($header) {
+    }))
+    .append(makeHeaderAnchor('hidden-md-up', function($header) {
+      return $header.attr('id');
+    }));
+  $('.anchorable')
+    .prepend(makeHeaderAnchor('hidden-sm-down', function($header) {
       return $header.parents('[id]').first().attr('id');
-    })
-  );
+    }))
+    .append(makeHeaderAnchor('hidden-md-up', function($header) {
+      return $header.parents('[id]').first().attr('id');
+    }));
 })();
